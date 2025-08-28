5e37f71d3542e7fd718087ade11b6ec552985aa5
Walassi Silva <91761745+WalassiSilva@users.noreply.github.com>
2025-08-28 11:19:43 +0000

    feat: add more message

--- script.js
+++ script.js
@@ -1,31 +1,31 @@
-const helmet = document.querySelector('.helmet');
-const star = document.querySelector('.s6');
-const message = document.querySelector('.message');
-const secret1 = document.querySelectorAll('.secret');
-const secret = document.querySelector('.secret');
-const btn = document.querySelector('.btn');
-const blackout = document.querySelector('.blackout');
-const star1 = document.querySelector('.s1');
+const helmet = document.querySelector(".helmet");
+const star = document.querySelector(".s6");
+const message = document.querySelector(".message");
+const secret1 = document.querySelectorAll(".secret");
+const secret = document.querySelector(".secret");
+const btn = document.querySelector(".btn");
+const blackout = document.querySelector(".blackout");
+const star1 = document.querySelector(".s1");
 
-helmet.addEventListener('click', () => {
-  message.style.display = 'block';  
+helmet.addEventListener("click", () => {
+  message.style.display = "block";
 });
-star.addEventListener('click', () => {
-  secret.style.display = 'block';  
-  console.log('secret');
+star.addEventListener("click", () => {
+  secret.style.display = "block";
+  console.log("secret");
   setTimeout(() => {
-    secret.style.display = 'none';
-  }, 1000);  
+    secret.style.display = "none";
+  }, 1000);
 });
 
-star1.addEventListener('click', () => {
-  secret1[1].style.display = 'block';  
-  console.log('secret');
+star1.addEventListener("click", () => {
+  secret1[1].style.display = "block";
+  console.log("secret");
   setTimeout(() => {
-    secret1[1].style.display = 'none';
-  }, 1000);  
+    secret1[1].style.display = "none";
+  }, 1000);
 });
 
-btn.addEventListener('click', () => {
-  blackout.style.display = 'none';
+btn.addEventListener("click", () => {
+  blackout.style.display = "none";
 });
